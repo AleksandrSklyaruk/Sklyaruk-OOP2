bb8dff108f2c5a61b434922fab7d039c70a56b0f
ads58 <ads58@W10792>
2026-02-04 19:59:42 +0700

    feature(lab2):выполнен первый пункт задания

---
+++ ClassLibrary1/Adult.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model 
+{
+    public class Adult : Person
+    {
+        /// <summary>
+        /// Серия паспорта
+        /// </summary>
+        private int _passportSeria;
+
+        /// <summary>
+        /// Номер паспорта
+        /// </summary>
+        private int _passportNumber;
+
+        /// <summary>
+        /// Семейное положение
+        /// </summary>
+        private MaritalStatus _maritalStatus;
+        
+        /// <summary>
+        /// Место работы
+        /// </summary>
+        private string _workPlace;
+
+        public Adult(string name, string surname, int age, Gender gender, 
+            int passportSeria, int passportNumber, MaritalStatus maritalStatus, string workPlace) : 
+            base(name, surname, age, gender) 
+        {
+            PassportSeria = passportSeria;
+            PassportNumber = passportNumber;
+            MaritalStatus = maritalStatus;
+            WorkPlace = workPlace;
+        }
+
+        /// <summary>
+        /// Конструктор по умолчанию
+        /// </summary>
+        public Adult() { }
+
+        /// <summary>
+        /// Минимальная серия паспорта
+        /// </summary>
+        public const int MinPassportSeria = 1000;
+
+        /// <summary>
+        /// Максимальная серия паспорта
+        /// </summary>
+        public const int MaxPassportSeria = 9999;
+        
+        /// <summary>
+        /// Минимальный номер паспорта
+        /// </summary>
+        public const int MinPassportNumber = 100000;
+
+        /// <summary>
+        /// Максимальный номер паспорта
+        /// </summary>
+        public const int MaxPassportNumber = 999999;
+
+        /// <summary>
+        /// Свойство позволяет получить или установить серию паспорта
+        /// </summary>
+        public int PassportSeria
+        {
+            get { return _passportSeria; }
+            set
+            {
+                if (string.IsNullOrEmpty(Convert.ToString(value)))
+                {
+                    throw new Exception("Введите номер серии!");
+                }
+
+                if (value < MinPassportSeria || value > MaxPassportSeria)
+                {
+                    throw new Exception($"{nameof(PassportSeria)} должен быть в дипазоне" +
+                        $" от {MinPassportSeria} до {MaxPassportSeria}");
+                }
+                _passportSeria = value;
+            }
+        }
+
+        /// <summary>
+        /// Свойство позволяет получить или установить номер паспорта
+        /// </summary>
+        public int PassportNumber
+        {
+            get { return _passportNumber; }
+            set
+            {
+                if (string.IsNullOrEmpty(Convert.ToString(value)))
+                {
+                    throw new Exception("Введите номер паспорта!");
+                }
+
+                if (value < MinPassportNumber || value > MaxPassportNumber)
+                {
+                    throw new Exception($"{nameof(PassportNumber)} должен быть в дипазоне" +
+                        $" от {MinPassportNumber} до {MaxPassportNumber}");
+                }
+                _passportNumber = value;
+            }
+        }
+
+        /// <summary>
+        /// Свойство позволяет получить или установить семейное положение 
+        /// </summary>
+        public MaritalStatus MaritalStatus
+        {
+            get { return _maritalStatus; }
+            set { _maritalStatus = value; }
+        }
+
+        /// <summary>
+        /// Ввод места работы
+        /// </summary>
+        public string WorkPlace
+        {
+            get { return _workPlace; }
+            set { _workPlace = value; }
+        }
+    }
+
+}
---
+++ ClassLibrary1/Child.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    internal class Child : Person
+    {
+        /// <summary>
+        /// Отец
+        /// </summary>
+        private Adult _father;
+
+        /// <summary>
+        /// Мама
+        /// </summary>
+        private Adult _mother;
+
+        /// <summary>
+        /// Школа.
+        /// </summary>
+        private string _school;
+
+        public Child(string name, string surname,
+            Gender gender, int age, Adult father, Adult mother,
+            string school) : base(name, surname, age, gender)
+        {
+            Father = father;
+            Mother = mother;
+            School = school;
+        }
+
+        /// <summary>
+        /// Отец ребенка
+        /// </summary>
+        public Adult Father
+        {
+            get { return _father; }
+            set { _father = value;}
+        }
+
+        /// <summary>
+        /// Мама ребенка
+        /// </summary>
+        public Adult Mother
+        {
+            get { return _mother; }
+            set { _mother = value; }
+        }
+
+        /// <summary>
+        /// Школа
+        /// </summary>
+        public string School
+        {
+            get { return _school; }
+            set { _school = value; }
+        }
+    }
+}
